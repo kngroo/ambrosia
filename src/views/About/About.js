b8fa91e0f8e569553e0cbf528184693bc6396673
Josh <josh@gfycat.com>
2016-10-07 03:35:30 -0700

    Make views more responsive with media queries

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -10,9 +10,9 @@ export default class About extends Component {
   render() {
     return (
       <section className="about-container">
-        <div className="row">
+        <div className="description">
           <p>
-          We are a local bakery located in the Lakeside neighborhood of San Francisco. We've been serving our loyal customers since 1988. The aroma of fresh-baked bread has delighted our neighborhood and the many passers-by for almost 25 years. We invite you to step in and enjoy our unique taste. We serve French pastries, cakes for all occasions, deli sandwiches and have a wonderful coffee bar. Seating is available. We look forward to seeing you!
+          We are a local bakery located in the Lakeside neighborhood of San Francisco. We've been serving our loyal customers since 1988. The aroma of fresh-baked bread has delighted our neighborhood and the many passers-by for almost 30 years. We invite you to step in and enjoy our unique taste. We serve French pastries, cakes for all occasions, deli sandwiches and have a wonderful coffee bar. Seating is available. We look forward to seeing you!
           </p>
         </div>
       </section>
