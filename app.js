4bbe74b92a426149dfb6edf5c1d147880471a83d
kngroo <joshkang@gmail.com>
2016-12-04 01:33:10 -0800

    Fix hmr in dev

--- app.js
+++ app.js
@@ -2,23 +2,7 @@ const Server = require('./server.js')
 const port = (process.env.PORT || 5000)
 const app = Server.app()
 
-if (process.env.NODE_ENV !== 'production') {
-  const webpack = require('webpack')
-  const webpackDevMiddleware = require('webpack-dev-middleware')
-  const webpackHotMiddleware = require('webpack-hot-middleware')
-  const config = require('./webpack.dev.config.js')
-  const compiler = webpack(config)
-
-  app.use(webpackHotMiddleware(compiler))
-  app.use(webpackDevMiddleware(compiler, {
-    noInfo: true,
-    publicPath: config.output.publicPath,
-    index: 'index.html'
-  }))
-}
-
 app.listen(port, () => {
   console.log('NODE_ENV:', process.env.NODE_ENV);
   console.log(`Listening at http://localhost:${port}`)
 });
-
--- server.js
+++ server.js
@@ -4,12 +4,27 @@ const express = require('express')
 module.exports = {
   app: function () {
     const app = express()
+
+  	if (process.env.NODE_ENV !== 'production') {
+	  const webpack = require('webpack')
+	  const webpackDevMiddleware = require('webpack-dev-middleware')
+	  const webpackHotMiddleware = require('webpack-hot-middleware')
+	  const config = require('./webpack.dev.config.js')
+	  const compiler = webpack(config)
+
+	  app.use(webpackDevMiddleware(compiler, {
+	    noInfo: true,
+	    publicPath: config.output.publicPath
+	  }))
+	  
+	  app.use(webpackHotMiddleware(compiler))
+	}
+
     const indexPath = path.join(__dirname, 'public', 'index.html')
     const publicPath = express.static(path.join(__dirname, 'public'))
 
     app.use('/public', publicPath)
     app.get('*', function (_, res) { res.sendFile(indexPath) })
-    app.get('/', function (_, res) { res.sendFile(indexPath) })
 
     return app
   }
