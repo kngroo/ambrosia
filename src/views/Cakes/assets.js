1ab291df0afa9f2e10abdb737448c21f2fe9e052
kngroo <joshkang@gmail.com>
2017-07-01 21:26:29 -0700

    Add cakes assets json

---
+++ src/views/Cakes/assets.js
@@ -0,0 +1,25 @@
+module.exports = {
+  cakes: [
+    {
+      name: 'Birthday Cake',
+      ingredients: 'White cake, peach & whipped cream filling, whipped cream frosting, topped with fruits'
+    },
+    {
+      name: 'Cappuccino Cakes',
+      ingredients: 'White cake, chocolate custard filling, cappucino whipped cream frosting'
+    },
+    {
+      name: 'Diplomat',
+      ingredients: 'White cake, custard with almond flavor filling & frosting, topped with fruits'
+    },
+    {
+      name: 'Fantasia',
+      ingredients: 'White cake, fresh strawberry, raspberry, blueberry, whipped cream filling, topped with white chocolate and mascarpone cheese'
+    },
+    {
+      name: 'Princess Cake',
+      ingredients: 'White cake, a layer of raspberry jam, a layer of custard topped with whipped cream and covered with marzipan'
+    }
+  ]
+};
+
