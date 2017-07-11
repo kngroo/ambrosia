e9584484ebd6f2f986185e14dca6810770671d87
kngroo <joshkang@gmail.com>
2017-07-11 01:13:04 -0700

    Update public dir

--- public/server.bundle.js
+++ public/server.bundle.js
@@ -141,9 +141,23 @@
 	      loader: 'style!css!sass' },
 	    // include: path.join(__dirname, 'src', 'styles') },
 	    { test: /\.css?$/,
-	      loader: 'style!css' }, { test: /\.png$/,
-	      loader: 'file' }, { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+	      loader: 'style!css' }, { test: /\.(gif|png|jpe?g|svg)$/i,
+	      loaders: ['file-loader?hash=sha512&digest=hex&name=[hash].[ext]', 'image-webpack-loader']
+	    }, { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
 	      loader: 'file' }]
+	  },
+
+	  imageWebpackLoader: {
+	    mozjpeg: {
+	      quality: 65
+	    },
+	    pngquant: {
+	      quality: "65-90",
+	      speed: 4
+	    },
+	    svgo: {
+	      plugins: [{ removeViewBox: false }, { removeEmptyAttrs: false }]
+	    }
 	  }
 	};
 	/* WEBPACK VAR INJECTION */}.call(exports, ""))
