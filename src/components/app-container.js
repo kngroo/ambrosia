7647f5f947da844f5efbe750de91966e388faa69
Josh Kang <josh.kang@udemy.com>
2020-07-07 00:27:54 -0700

    Remove covid notice

--- src/components/app-container.js
+++ src/components/app-container.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Footer from './footer';
 import Header from './header';
 import Navbar from './navbar';
-import Notice from './notice';
 
 const AppContainer = ({ children }) => (
   <div>
-    <Notice />
     <Header />
     <Navbar />
     <div className="container">{children}</div>
